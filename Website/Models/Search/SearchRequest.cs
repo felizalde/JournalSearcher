fd4976090e04479754bb4c46d4977ec11fd34929
Federico Elizalde <federico.elizalde@empactit.com>
2022-01-04 20:43:21 -0300

    limit abstract length

--- Website/Models/Search/SearchRequest.cs
+++ Website/Models/Search/SearchRequest.cs
@@ -4,6 +4,7 @@ using Common.Models;
 namespace Website.Models.Search;
 
 public record SearchRequest([Required] string Title, 
-    [Required] string Abstract, 
+    [Required]
+    [MaxLength(8000)] string Abstract, 
     List<string> Keywords, 
     List<RefineItem> Setting);
